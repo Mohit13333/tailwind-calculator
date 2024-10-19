555ec21e59be51bb4708da617443e9760c1f14b9
MOHIT SINGH <92773869+Mohit13333@users.noreply.github.com>
2024-10-20 01:17:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-const display=document.getElementById("display");
-function appendToDisplay(input){
- display.value+=input;
- 
-}
-function clearDisplay(){
-    display.value="";
-}
-    function calculate() {
-        try{
-        display.value=eval(display.value); 
+const display = document.getElementById('display');
+
+        document.getElementById('clearDisplay').addEventListener('click', clearDisplay);
+        document.getElementById('deleteDisplay').addEventListener('click', deleteDisplay);
+        document.getElementById('percentBtn').addEventListener('click', () => appendToDisplay('%'));
+        document.getElementById('divideBtn').addEventListener('click', () => appendToDisplay('/'));
+        document.getElementById('multiplyBtn').addEventListener('click', () => appendToDisplay('*'));
+        document.getElementById('subtractBtn').addEventListener('click', () => appendToDisplay('-'));
+        document.getElementById('addBtn').addEventListener('click', () => appendToDisplay('+'));
+        document.getElementById('equalBtn').addEventListener('click', calculateResult);
+        document.getElementById('dotBtn').addEventListener('click', () => appendToDisplay('.'));
+        document.getElementById('zeroBtn').addEventListener('click', () => appendToDisplay('0'));
+        document.getElementById('oneBtn').addEventListener('click', () => appendToDisplay('1'));
+        document.getElementById('twoBtn').addEventListener('click', () => appendToDisplay('2'));
+        document.getElementById('threeBtn').addEventListener('click', () => appendToDisplay('3'));
+        document.getElementById('fourBtn').addEventListener('click', () => appendToDisplay('4'));
+        document.getElementById('fiveBtn').addEventListener('click', () => appendToDisplay('5'));
+        document.getElementById('sixBtn').addEventListener('click', () => appendToDisplay('6'));
+        document.getElementById('sevenBtn').addEventListener('click', () => appendToDisplay('7'));
+        document.getElementById('eightBtn').addEventListener('click', () => appendToDisplay('8'));
+        document.getElementById('nineBtn').addEventListener('click', () => appendToDisplay('9'));
+
+        function appendToDisplay(value) {
+            display.value += value;
         }
-        catch(error) {
-            display.value="Error";
+
+        function clearDisplay() {
+            display.value = '';
         }
-      }
+
+        function deleteDisplay() {
+            display.value = display.value.slice(0, -1);
+        }
+
+        function calculateResult() {
+            try {
+                display.value = eval(display.value);
+            } catch (err) {
+                alert('Invalid operation');
+            }
+        }
